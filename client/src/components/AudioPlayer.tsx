0e0b336a1db1f8888db7dbf0e7ba3a9de0c542bc
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2022-10-01 15:05:42 +0200

    Convert to typescript

---
+++ client/src/components/AudioPlayer.tsx
@@ -0,0 +1,107 @@
+import React, { useRef, useState, useEffect } from "react";
+import { Track } from "../models/Track";
+import styles from "./AudioPlayer.module.css";
+
+type AudioEvent = Event & {
+	target: { currentTime: number; duration: number; value: number };
+};
+
+const AudioPlayer = ({ track }: { track: Track }) => {
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [progress, setProgress] = useState(0);
+	const audioRef = useRef<HTMLAudioElement>({} as HTMLAudioElement);
+
+	const handlePlay = () => {
+		setIsPlaying(true);
+	};
+
+	const handlePause = () => {
+		setIsPlaying(false);
+	};
+
+	const handleTimeUpdate = (e: any) => {
+		setProgress(e.target.currentTime / e.target.duration);
+	};
+
+	const handleSliderChange = (e: AudioEvent) => {
+		audioRef.current.currentTime =
+			(e.target.value / 1000) * audioRef.current.duration;
+	};
+
+	const handleTogglePlaybackClick = () => {
+		if (audioRef.current.paused) {
+			audioRef.current.play();
+		} else {
+			audioRef.current.pause();
+		}
+	};
+
+	useEffect(() => {
+		console.log(audioRef.current);
+		audioRef.current.addEventListener("play", handlePlay);
+		audioRef.current.addEventListener("pause", handlePause);
+		audioRef.current.addEventListener("timeupdate", handleTimeUpdate);
+	}, []);
+
+	useEffect(() => {
+		audioRef.current.play();
+		audioRef.current.currentTime = 0;
+	}, [track]);
+
+	return (
+		<>
+			<audio src={track.audio} ref={audioRef} />
+			<div className={styles.audioPlayer}>
+				<button
+					className={styles.togglePlaybackButton}
+					onClick={handleTogglePlaybackClick}
+				>
+					{isPlaying ? (
+						<svg
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fillRule="evenodd"
+								clipRule="evenodd"
+								d="M10 5H7V19H10V5ZM17 5H14V19H17V5Z"
+								fill="#000"
+							/>
+						</svg>
+					) : (
+						<svg
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path d="M20 12L8 5V19L20 12Z" fill="#000" />
+						</svg>
+					)}
+				</button>
+				<div className={styles.trackInfo}>
+					<div className={styles.trackTitle}>{track.title}</div>
+					<div className={styles.trackArtist}>
+						{track.main_artists.join(", ")}
+					</div>
+				</div>
+				<div className={styles.sliderContainer}>
+					<input
+						type="range"
+						min="1"
+						max="1000"
+						value={progress * 1000}
+						className={styles.slider}
+						onChange={handleSliderChange as any}
+					/>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default AudioPlayer;
---
+++ client/src/models/Track.ts
@@ -0,0 +1,14 @@
+export interface Track {
+	audio: string;
+	bpm: number;
+	cover_art: string;
+	featured_artists: string[];
+	genres: string[];
+	id: string;
+	length: number;
+	main_artists: string[];
+	moods: string[];
+	spotify: string;
+	title: string;
+	waveform: string;
+}
